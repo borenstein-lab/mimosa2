d81023b2916220175b1e0c4a2d2109b886824c32
cnoecker <cecilia.noecker@gmail.com>
2021-12-05 17:26:46 -0800

    fix typo

--- R/speciesPicrustFunctions.R
+++ R/speciesPicrustFunctions.R
@@ -56,7 +56,7 @@ met_table_fix = function(met_table, nzero_filt = 5){
   }
   ## Check for large integers in mets
   met_col_classes <- unlist(met_table[,lapply(.SD, class)])
-  if(any(met_col_classes) == "integer64"){ 
+  if(any(met_col_classes == "integer64")){ 
     warning("Metabolite table has large integers in table, entire dataset will be coerced to integer64")
     met_table = data.table(compound = met_table[,compound], met_table[,lapply(.SD, bit64::as.integer64), 
                                                        .SDcols = names(met_table)[2:ncol(met_table)]])
